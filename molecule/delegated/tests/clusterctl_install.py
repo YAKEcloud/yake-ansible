7961fc9586a39835d951c6c63cf34884b7682c7a
Christian Berendt <berendt@23technologies.cloud>
2025-01-20 09:41:01 +0100

    Add clusterctl_install role (#13)
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

---
+++ molecule/delegated/tests/clusterctl_install.py
@@ -0,0 +1,6 @@
+def test_clusterctl_install_script(host):
+    f = host.file("/usr/local/bin/clusterctl")
+    assert f.exists
+    assert not f.is_directory
+    assert f.mode == 0o755
+    assert "#!/usr/bin/env bash" in f.content_string

6596468229908ceae3282c27ec444cf26442eac9
Christian Berendt <berendt@23technologies.cloud>
2025-01-20 13:57:42 +0100

    Add kubectl_install role (#19)
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

---
+++ molecule/delegated/tests/kubectl_install.py
@@ -0,0 +1,12 @@
+def test_kubectl_install_script(host):
+    f = host.file("/usr/local/bin/kubectl")
+    assert f.exists
+    assert not f.is_directory
+    assert f.mode == 0o755
+    assert "#!/usr/bin/env bash" in f.content_string
+
+
+def test_kubectl_install_container(host):
+    container_name = "kubectl"
+    container = host.docker(container_name)
+    assert container.is_running

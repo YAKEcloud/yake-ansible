213092411fd2d099953dca7ab99199db853776ff
Christian Berendt <berendt@23technologies.cloud>
2025-01-29 19:29:45 +0100

    Overall improvements (#44)
    * get capi kubeconfig
    * use per cluster clouds.yaml
    * change name of kind cluster
    
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

--- molecule/delegated/tests/kind_cluster.py
+++ molecule/delegated/tests/kind_cluster.py
@@ -1,4 +1,4 @@
 def test_kind_cluster_container(host):
-    container_name = "yake-control-plane"
+    container_name = "clusterapi-control-plane"
     container = host.docker(container_name)
     assert container.is_running

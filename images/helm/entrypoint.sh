e70829b1c1239b2ae0ecb2f0dd7951e980f1927b
Christian Berendt <berendt@23technologies.cloud>
2025-01-31 21:26:25 +0100

    Add helm container image (#93)
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

---
+++ images/helm/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -n "${USER_ID}" ] && [ -n "${GROUP_ID}" ]; then
+    groupmod -g ${GROUP_ID} ${USER_NAME} 2>/dev/null
+    usermod -u ${USER_ID} -g ${GROUP_ID} ${USER_NAME} 2>/dev/null
+    chown -R ${USER_ID}:${GROUP_ID} /${USER_NAME}
+fi
+
+if [ -n "${DOCKER_GROUP_ID}" ]; then
+    groupmod -g ${DOCKER_GROUP_ID} ${DOCKER_GROUP_NAME} 2>/dev/null
+fi
+
+exec /usr/bin/dumb-init -- su - "$USER_NAME" -c "$*"

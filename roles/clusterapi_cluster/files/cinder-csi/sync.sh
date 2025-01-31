e26ec556ba1ecdfc55c2c1f83d9c935770b6e5fb
Christian Berendt <berendt@23technologies.cloud>
2025-01-31 14:10:24 +0100

    Use gilt insteaf of sync scripts
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

--- roles/clusterapi_cluster/files/cinder-csi/sync.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION=v1.31.2
-
-for file in cinder-csi-controllerplugin-rbac.yaml cinder-csi-controllerplugin.yaml cinder-csi-nodeplugin-rbac.yaml cinder-csi-nodeplugin.yaml csi-cinder-driver.yaml; do
-    curl -o $file https://raw.githubusercontent.com/kubernetes/cloud-provider-openstack/refs/tags/$VERSION/manifests/cinder-csi-plugin/$file
-done
--- roles/clusterapi_cluster/files/cloud-controller-manager/sync.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION=v1.31.2
-
-for file in cloud-controller-manager-role-bindings.yaml cloud-controller-manager-roles.yaml openstack-cloud-controller-manager-ds.yaml; do
-    curl -o $file https://raw.githubusercontent.com/kubernetes/cloud-provider-openstack/refs/tags/$VERSION/manifests/controller-manager/$file
-done

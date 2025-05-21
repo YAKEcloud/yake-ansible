30b1eb31d63017875b2adb43728e703e417b8abf
Marius <58997736+rhizoet@users.noreply.github.com>
2025-05-21 19:40:34 +0000

    Use yake- prefix for all container wrapper scripts (#189)
    Signed-off-by: Marius <wernicke@23technologies.cloud>

--- molecule/delegated/tests/kubectl_install.py
+++ molecule/delegated/tests/kubectl_install.py
@@ -1,12 +1,12 @@
-def test_clusterctl_install_script(host):
-    f = host.file("/usr/local/bin/yake-clusterctl")
+def test_kubectl_install_script(host):
+    f = host.file("/usr/local/bin/yake-kubectl")
     assert f.exists
     assert not f.is_directory
     assert f.mode == 0o755
     assert "#!/usr/bin/env bash" in f.content_string
 
 
-def test_clusterctl_install_container(host):
-    container_name = "clusterctl"
+def test_kubectl_install_container(host):
+    container_name = "kubectl"
     container = host.docker(container_name)
     assert container.is_running
